dd183f290bc3ea703b488370d22cb1929c0ad04a
chrillah <ch.ronnberg@gmail.com>
2023-05-09 11:25:45 +0200

    third commit

--- server.ts
+++ server.ts
@@ -1,15 +1,30 @@
 import express, {Request, Response, json} from 'express'
 import carRouter from './routes/carRoutes'
 
+import {getCars, createCar} from './db/models/carController'
+
 import {connect} from 'mongoose'
 
-connect('mongodb://localhost:27017/codealongmern')
+connect('mongodb://localhost:27017/test')
 
 const app = express()
 
 const port = 8000
 
 app.use(json())
+
+app.use('/car', carRouter)
+
+app.get('/car' , async(req:Request, res:Response)=>{
+    const cars = await getCars()
+    res.json(cars)
+})
+
+app.post('/car', async (req:Request, res:Response)=>{
+    const newCar = req.body
+    const createdCar = await createCar(newCar)
+    res.json(createdCar)
+})
 
 app.get('/', (req:Request, res:Response)=>{
     res.send('Ja detta fungerar')
